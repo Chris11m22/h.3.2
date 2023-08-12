902a218b0597c482ef9d764f786382fa97869f61
christinam <malininak11@gmail.com>
2023-08-12 12:40:38 +0500

    h3/2//

--- src/main/java/ru/hogwarts/demo/school/h/scripts42.sql
+++ src/main/java/ru/hogwarts/demo/school/h/scripts42.sql
@@ -1,14 +1,14 @@
-CREATE TABLE users (
+CREATE TABLE student (
     ...,
     age INTEGER CHECK (age > 16),
     ...
 );
-CREATE TABLE users (
+CREATE TABLE student (
     ...,
     name TEXT UNIQUE NOT NULL,
     ...
 );
-CREATE TABLE users (
+CREATE TABLE student (
     ...,
     faculty TEXT UNIQUE,
     ...
